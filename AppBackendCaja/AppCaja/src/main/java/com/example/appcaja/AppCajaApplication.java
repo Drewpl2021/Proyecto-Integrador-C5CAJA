ab7e385bf7e7afc8fa839118a097a0d24d8093f0
Drewpl2021 <linoplas0@gmail.com>
2024-06-10 10:02:27 -0500

    yufjguyvjh

---
+++ AppBackendCaja/AppCaja/src/main/java/com/example/appcaja/AppCajaApplication.java
@@ -0,0 +1,13 @@
+package com.example.appcaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCajaApplication.class, args);
+	}
+
+}
